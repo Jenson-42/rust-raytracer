8cbdef65b52d0b8723afeb41f435513ca21d8ec5
Jenson-42 <76667569+Jenson-42@users.noreply.github.com>
2023-08-08 19:39:57 +0100

    remove illegal spheres from random world example

--- src/random_world.rs
+++ src/random_world.rs
@@ -26,10 +26,16 @@ pub fn random_world() -> Vec<ArcHittable> {
             Lambertian::new(Colour::new(0.9, 0.9, 0.9)).into(),
         )
         .into(),
-        // Lambertian::new_arc(Colour::new(0.5, 0.5, 0.5)),
         Lambertian::new(Colour::new(0.4, 0.2, 0.1)).into(),
         Dielectric::new(1.5).into(),
         Metal::new(Colour::new(0.7, 0.6, 0.5), 0.0).into(),
+    ];
+
+    // Locations of the spheres so they can be avoided by the smaller ones.
+    let ball_locations = vec![
+        Point3::new(-4.0, 1.0, 0.0),
+        Point3::new(0.0, 1.0, 0.0),
+        Point3::new(4.0, 1.0, 0.0),
     ];
 
     // Create a scene with a ground as well as a sphere for each material.
@@ -43,40 +49,50 @@ pub fn random_world() -> Vec<ArcHittable> {
         )
         .into(),
         // Matte Ball.
-        Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, &materials[1]).into(),
+        Sphere::new(ball_locations[0], 1.0, &materials[1]).into(),
         // Glass ball.
-        Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, &materials[2]).into(),
+        Sphere::new(ball_locations[1], 1.0, &materials[2]).into(),
         // Metal ball.
-        Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, &materials[3]).into(),
+        Sphere::new(ball_locations[2], 1.0, &materials[3]).into(),
     ];
 
     let mut rng = rand::thread_rng();
 
     for a in -11..11 {
         for b in -11..11 {
+            let radius = 0.1 + (random_f64(&mut rng) / 8.0);
+
             let center = Point3::new(
                 a as f64 + 0.9 * random_f64(&mut rng),
-                0.2,
+                radius,
                 b as f64 + 0.9 * random_f64(&mut rng),
             );
 
-            if (center - Point3::new(4.0, 0.2, 0.0)).length() < 0.9 {
+            // Discard any spheres not on the disk.
+            if center.length() > 15.0 {
                 continue;
             }
 
+            // Discard any spheres touching the big ones.
+            if ball_locations.iter().any(|p| (center - *p).length() < 1.1) {
+                continue;
+            }
+
+            // Choose a random material for the sphere.
             let material: ArcMaterial = match random_f64(&mut rng) {
-                mat if mat < 0.8 => {
+                mat if mat < 0.9 => {
                     let albedo = Colour::random(&mut rng);
                     Lambertian::new(albedo).into()
                 }
                 mat if mat < 0.95 => {
                     let albedo = Colour::random(&mut rng);
-                    Lambertian::new(albedo).into()
+                    Metal::new(albedo, 0.2).into()
                 }
                 _ => Dielectric::new(1.5).into(),
             };
 
-            world.push(Sphere::new(center, 0.2, &material).into());
+            // Push the new sphere into the world.
+            world.push(Sphere::new(center, radius, &material).into());
         }
     }
 
